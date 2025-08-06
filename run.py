3c92ebcbddbcd3ecefa7ebed8f5b3a2bad37c32c
Yessha-bapna <bapnayessha05@gmail.com>
2025-08-06 15:23:16 +0530

    run

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 # run.py
-import os
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    # Render provides a $PORT environment variable
-    port = int(os.environ.get("PORT", 8000))  # Default to 8000 for local testing
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 for local dev
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
         port=port,
-        reload=bool(os.environ.get("RELOAD", False))  # Reload only if RELOAD env is set
+        reload=False
     )
