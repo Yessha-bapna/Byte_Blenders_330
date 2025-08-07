58d0d0b2bb2319ba5691fe9ea1a005512b39cda4
Yessha-bapna <bapnayessha05@gmail.com>
2025-08-07 10:52:26 +0530

    o

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
+from fastapi import FastAPI
 import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Backend running successfully"}
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Get PORT from env (Render will set it)
+    uvicorn.run("run:app", host="0.0.0.0", port=port)
